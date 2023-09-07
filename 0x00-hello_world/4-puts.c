8aeeba18c03d06c04bac2b41a2e12c4403ff8bf6
hope14657262 <hopemelese89@gmail.com>
2023-09-07 14:55:16 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - this is the main put function
+ * main - A program that prints a line with  puts  the function
  * Retrun: 0 (success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0)
 }
