5da82d8db1aee23ae8edf50b1b79e9cba0cd1cc5
hope14657262 <hopemelese89@gmail.com>
2023-09-08 18:48:39 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is the function of task 7
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
