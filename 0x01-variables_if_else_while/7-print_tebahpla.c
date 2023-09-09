33694ce42e085d75544a5ef51ae26b8b4d528b21
hope14657262 <hopemelese89@gmail.com>
2023-09-08 18:40:10 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the function of task 7
+ * Return: always 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
