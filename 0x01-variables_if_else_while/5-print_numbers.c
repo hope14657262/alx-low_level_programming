312bd1d92a15ac73d5802b8debd17bb223f3872e
hope14657262 <hopemelese89@gmail.com>
2023-09-08 18:06:38 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print number 0 to 9
+ * Return: always 0
+ */
+int main(void)
+{
+char i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
