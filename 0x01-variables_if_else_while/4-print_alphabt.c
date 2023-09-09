93df1521b2e670508d10b7aebfe589933629dd6a
hope14657262 <hopemelese89@gmail.com>
2023-09-08 17:42:54 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,8 @@ int main(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
+if (i != 'e' && i != 'q')
 putchar(i);
-{
-if (i == 'e' || i == 'q')
-putchar(i);
-else
-putchar(i);
-}
 putchar('\n');
 return (0);
 }
