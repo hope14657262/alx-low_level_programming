e3a144d428522bad167846a0dec6a7ace8c337a3
hope14657262 <hopemelese89@gmail.com>
2023-09-08 17:01:50 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is the function of task three
+ * Return: always 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+{
+if (i == 'e' || i == 'q')
+putchar(i);
+else
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
