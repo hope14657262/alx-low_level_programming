68e2251dd63d90ccbf819f5b71c1605e57433109
hope14657262 <hopemelese89@gmail.com>
2023-09-08 18:19:39 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the function of task 6
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+for(i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
