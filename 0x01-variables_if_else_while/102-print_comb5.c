e654aa76638a1220a4b051be45a84fea7a069850
hope14657262 <hopemelese89@gmail.com>
2023-09-12 14:22:19 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,9 +15,9 @@ if (i < j)
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(' ');
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
-if (i !=98 || j != 99)
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
