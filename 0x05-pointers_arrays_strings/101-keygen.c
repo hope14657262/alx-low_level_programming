065ace66914dccce940bcc9129b476e2c737e3db
hope14657262 <hopemelese89@gmail.com>
2023-09-18 19:01:01 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,40 +4,26 @@
 /**
  * main - genereate random valid password for the
  * program: 101-crackme
- * Return: always 0
+ * Return: 0 (success)
  */
 int main(void)
 {
-char password[84];
-int index = 0, sum = 0, diff_half1, diff_half2;
-srand(time(0));
-while (sum < 2772)
+int pass[100];
+int i, sum, n;
+sum = 0;
+srand(time(NULL));
+for (i = 0; i < 100; i++)
 {
-password[index] = 33 + rand() % 94;
-sum += password[index++];
-}
-password[index] = '\0';
-if (sum != 2772)
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) -'0' < 78)
 {
-diff_half1 = (sum - 2772) / 2;
-diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 != 0)
-diff_half1++;
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half1))
-{
-password[index] -= diff_half1;
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
 break;
 }
 }
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half2))
-password[index] -= diff_half2;
-break;
-}
-}
-printf("%s", password);
 return (0);
 }
