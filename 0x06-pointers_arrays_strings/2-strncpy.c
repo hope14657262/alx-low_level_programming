594531c497d202d5450ecadf0dff5f0c45ae9504
hope14657262 <hopemelese89@gmail.com>
2023-09-21 15:44:21 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - concatenates two strings
+ * @dest: value one
+ * @src: value two
+ * @n: value three
+ * Return: alyaws 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j]
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
+}
