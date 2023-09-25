3df435cf17beb030ab551d19e8a2b583ecff6099
hope14657262 <hopemelese89@gmail.com>
2023-09-25 15:28:37 -0700

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,21 +4,26 @@
  * _strspn - prints buffer in hexa
  * @accept:parameter accept
  * @s:parameter s
- * Return: 0
+ * Return: the number of bytes in s which
+ * consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, n, value, check;
-value = 0;
-for (i = 0; s[i] != '\0'; i++)
+unsigned int bytes = 0;
+int index;
+while (*s)
 {
-check = 0;
-for (n = 0; accept[n] != '\0'; n++)
+for (index = 0; accept[index]; index++)
 {
-if (accept[n] == s[i])
+if (*s == accept[index])
 {
-value++;
-check = 1;
+bytes++;
+break;
 }
+else if (accept[index + 1] == '\0')
+return (bytes);
 }
-if (check == 0)
+s++;
+}
+return (0);
+}
